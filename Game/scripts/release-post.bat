1de76242914bc6f8c873c346e1c33abc8f582e5d
DylanCheetah <dylan_the_cheetah@outlook.com>
2019-04-04 18:25:52 -0400

    Added the new src files.

---
+++ Game/scripts/release-post.bat
@@ -0,0 +1,18 @@
+echo Copying libraries...
+xcopy ..\deps\win32\*.dll ..\bin\Release\client\ /Y
+xcopy ..\deps\win32\Ogre\bin\*.dll ..\bin\Release\client\ /Y
+xcopy ..\deps\win32\Ogre\bin\*.cfg ..\bin\Release\client\ /Y
+xcopy ..\deps\win32\RakNet\bin\*.dll ..\bin\Release\client\ /Y
+xcopy ..\deps\win32\fmod\bin\*.dll ..\bin\Release\client\ /Y
+
+echo Copying data files...
+xcopy ..\static\client\config\*.cfg ..\bin\Release\client\ /Y
+xcopy ..\static\client\text\*.txt ..\bin\Release\client\ /Y
+xcopy ..\static\client\default\*.dat ..\bin\Release\client\ /Y
+xcopy ..\data ..\bin\Release\client\data\ /S /E /Y
+del ..\bin\Release\client\localIP.txt
+
+echo Creating directories...
+mkdir ..\bin\Release\client\screenshots
+
+echo Done.
---
+++ LoginServer/scripts/release-post.bat
@@ -0,0 +1,3 @@
+xcopy ..\deps\win32\*.dll ..\bin\Release\server\ /Y
+xcopy ..\deps\win32\RakNet\bin\*.dll ..\bin\Release\server\ /Y
+xcopy ..\static\server\Data ..\bin\Release\server\Data\ /E /S /Y
